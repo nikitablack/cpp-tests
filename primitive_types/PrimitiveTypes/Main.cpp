5248a2ddcfc9f0abe376e199658be31d2193eef9
Nikita <nikita_chernyi@mail.ru>
2014-06-25 12:25:10 +0200

    Add int and float

--- primitive_types/PrimitiveTypes/Main.cpp
+++ primitive_types/PrimitiveTypes/Main.cpp
@@ -5,49 +5,161 @@ using namespace std;
 
 int main(){
 	// char
-	char char1{ 50 };
+	/*char char1{ 50 };
 	cout << char1 << endl;
 
 	char char2{ 'a' };
 	cout << char2 << endl;
 
-	char char3{ '\xFF' };
+	char char3{ '\x8F' };
 	cout << char3 << endl;
 
 	cout << "size of char: " << sizeof(char) << endl;
 
 	std::cout << "min char value: " << (int)std::numeric_limits<char>::min() << std::endl;
-	std::cout << "max char value: " << (int)std::numeric_limits<char>::max() << std::endl;
+	std::cout << "max char value: " << (int)std::numeric_limits<char>::max() << std::endl << std::endl;*/
 
 	// signed char
-
-	std::cout << "min signed char value: " << (int)std::numeric_limits<signed char>::min() << std::endl;
-	std::cout << "max signed char value: " << (int)std::numeric_limits<signed char>::max() << std::endl;
+	/*std::cout << "min signed char value: " << (int)std::numeric_limits<signed char>::min() << std::endl;
+	std::cout << "max signed char value: " << (int)std::numeric_limits<signed char>::max() << std::endl << std::endl;*/
 
 	// unsigned char
-	std::cout << "min unsigned char value: " << (int)std::numeric_limits<unsigned char>::min() << std::endl;
-	std::cout << "max unsigned char value: " << (int)std::numeric_limits<unsigned char>::max() << std::endl;
+	/*std::cout << "min unsigned char value: " << (int)std::numeric_limits<unsigned char>::min() << std::endl;
+	std::cout << "max unsigned char value: " << (int)std::numeric_limits<unsigned char>::max() << std::endl << std::endl;*/
 
 	// wchar_t
-	wchar_t char4{ L'\xFFFF' };
+	/*wchar_t char4{ L'\xFFFF' };
 	cout << char4 << endl;
 
 	cout << "size of wchar_t: " << sizeof(wchar_t) << endl;
 
 	std::cout << "min wchar_t value: " << (int)std::numeric_limits<wchar_t>::min() << std::endl;
-	std::cout << "max wchar_t value: " << (int)std::numeric_limits<wchar_t>::max() << std::endl;
+	std::cout << "max wchar_t value: " << (int)std::numeric_limits<wchar_t>::max() << std::endl << std::endl;*/
 
 	// char16_t
-	cout << "size of char16_t: " << sizeof(char16_t) << endl;
+	/*cout << "size of char16_t: " << sizeof(char16_t) << endl;
 
 	std::cout << "min char16_t value: " << (int)std::numeric_limits<char16_t>::min() << std::endl;
-	std::cout << "max char16_t value: " << (int)std::numeric_limits<char16_t>::max() << std::endl;
+	std::cout << "max char16_t value: " << (int)std::numeric_limits<char16_t>::max() << std::endl << std::endl;*/
 
 	// char32_t
-	cout << "size of char32_t: " << sizeof(char32_t) << endl;
+	/*cout << "size of char32_t: " << sizeof(char32_t) << endl;
 
 	std::cout << "min char32_t value: " << (int)std::numeric_limits<char32_t>::min() << std::endl;
-	std::cout << "max char32_t value: " << (int)std::numeric_limits<char32_t>::max() << std::endl; // -1 wtf?????
+	std::cout << "max char32_t value: " << (int)std::numeric_limits<char32_t>::max() << std::endl << std::endl; // -1 wtf?????*/
+
+	// int
+	/*int int1{ 50 };
+	cout << int1 << endl;
+
+	cout << "size of int: " << sizeof(int) << endl;
+
+	std::cout << "min int value: " << (int)std::numeric_limits<int>::min() << std::endl;
+	std::cout << "max int value: " << (int)std::numeric_limits<int>::max() << std::endl << std::endl;*/
+
+	// signed int
+	/*std::cout << "min signed int value: " << (signed int)std::numeric_limits<signed int>::min() << std::endl;
+	std::cout << "max signed int value: " << (signed int)std::numeric_limits<signed int>::max() << std::endl << std::endl;*/
+
+	// unsigned int
+	/*unsigned  int int2{ 50U };
+	cout << int2 << endl;
+
+	std::cout << "min unsigned int value: " << (unsigned int)std::numeric_limits<unsigned int>::min() << std::endl;
+	std::cout << "max unsigned int value: " << (unsigned int)std::numeric_limits<unsigned int>::max() << std::endl << std::endl;*/
+
+	// short int
+	/*cout << "size of short int: " << sizeof(short int) << endl;
+
+	std::cout << "min short int value: " << (short int)std::numeric_limits<short int>::min() << std::endl;
+	std::cout << "max short int value: " << (short int)std::numeric_limits<short int>::max() << std::endl << std::endl;*/
+
+	// signed short int
+	/*std::cout << "min signed short int value: " << (signed short int)std::numeric_limits<signed short int>::min() << std::endl;
+	std::cout << "max signed short int value: " << (signed short int)std::numeric_limits<signed short int>::max() << std::endl << std::endl;*/
+
+	// unsigned short int
+	/*unsigned short int int3{ 50000U };
+	cout << int3 << endl;
+
+	std::cout << "min unsigned short int value: " << (unsigned short int)std::numeric_limits<unsigned short int>::min() << std::endl;
+	std::cout << "max unsigned short int value: " << (unsigned short int)std::numeric_limits<unsigned short int>::max() << std::endl << std::endl;*/
+
+	// long int
+	/*long int int4{ 50L };
+	cout << int4 << endl;
+
+	cout << "size of long int: " << sizeof(long int) << endl;
+
+	std::cout << "min long int value: " << (long int)std::numeric_limits<long int>::min() << std::endl;
+	std::cout << "max long int value: " << (long int)std::numeric_limits<long int>::max() << std::endl << std::endl;*/
+
+	// long signed int
+	/*std::cout << "min long signed int value: " << (long signed int)std::numeric_limits<long signed int>::min() << std::endl;
+	std::cout << "max long signed int value: " << (long signed int)std::numeric_limits<long signed int>::max() << std::endl << std::endl;*/
+
+	// long unsigned int
+	/*long unsigned int int5{ 50LU };
+	cout << int5 << endl;
+
+	std::cout << "min long unsigned int value: " << (long unsigned int)std::numeric_limits<long unsigned int>::min() << std::endl;
+	std::cout << "max long unsigned int value: " << (long unsigned int)std::numeric_limits<long unsigned int>::max() << std::endl << std::endl;*/
+
+	// long long int
+	/*long long int int6{ 50LL };
+	cout << int6 << endl;
+
+	cout << "size of long long int: " << sizeof(long long int) << endl;
+
+	std::cout << "min long long int value: " << (long long int)std::numeric_limits<long long int>::min() << std::endl;
+	std::cout << "max long long int value: " << (long long int)std::numeric_limits<long long int>::max() << std::endl << std::endl;*/
+
+	// long long signed int
+	/*std::cout << "min long long signed int value: " << (long long signed int)std::numeric_limits<long long signed int>::min() << std::endl;
+	std::cout << "max long long signed int value: " << (long long signed int)std::numeric_limits<long long signed int>::max() << std::endl << std::endl;*/
+
+	// long long unsigned int
+	/*long long unsigned int int7{ 50LLU };
+	cout << int7 << endl;
+
+	std::cout << "min long long unsigned int value: " << (long long unsigned int)std::numeric_limits<long long unsigned int>::min() << std::endl;
+	std::cout << "max long long unsigned int value: " << (long long unsigned int)std::numeric_limits<long long unsigned int>::max() << std::endl << std::endl;*/
+
+	// float
+	float float1{ 5.0F };
+	cout << float1 << endl;
+
+	cout << "size of float: " << sizeof(float) << endl;
+
+	std::cout << "min float value: " << (float)std::numeric_limits<float>::min() << std::endl;
+	std::cout << "max float value: " << (float)std::numeric_limits<float>::max() << std::endl << std::endl;
+
+	// long float
+	long float float2{ 5.0F };
+	cout << float2 << endl;
+
+	cout << "size of long float: " << sizeof(long float) << endl;
+
+	std::cout << "min long float value: " << (long float)std::numeric_limits<long float>::min() << std::endl;
+	std::cout << "max long float value: " << (long float)std::numeric_limits<long float>::max() << std::endl << std::endl;
+
+	// double
+	double double1{ 5.0 };
+	cout << double1 << endl;
+
+	cout << "size of double: " << sizeof(double) << endl;
+
+	std::cout << "min double value: " << (double)std::numeric_limits<double>::min() << std::endl;
+	std::cout << "max double value: " << (double)std::numeric_limits<double>::max() << std::endl << std::endl;
+
+	// long double
+	long double double2{ 5.0L };
+	cout << double2 << endl;
+
+	cout << "size of long double: " << sizeof(long double) << endl;
+
+	std::cout << "min long double value: " << (long double)std::numeric_limits<long double>::min() << std::endl;
+	std::cout << "max long double value: " << (long double)std::numeric_limits<long double>::max() << std::endl << std::endl;
 
 	int a;
 	cin >> a;
